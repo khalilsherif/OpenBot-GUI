08e2bd715a5d7405a89e8c2155dba1c6e3466e47
Khalil Sherif <sherif.khalil@live.ca>
2016-05-04 17:05:42 -0400

    Initial commit

---
+++ OpenBot/frmMain.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace OpenBot
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstActiveHandlers = new System.Windows.Forms.ListBox();
+            this.cmsFactories = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtOauth = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtChannel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstActiveHandlers
+            // 
+            this.lstActiveHandlers.ContextMenuStrip = this.cmsFactories;
+            this.lstActiveHandlers.FormattingEnabled = true;
+            this.lstActiveHandlers.Location = new System.Drawing.Point(12, 12);
+            this.lstActiveHandlers.Name = "lstActiveHandlers";
+            this.lstActiveHandlers.Size = new System.Drawing.Size(218, 368);
+            this.lstActiveHandlers.TabIndex = 0;
+            // 
+            // cmsFactories
+            // 
+            this.cmsFactories.Name = "cmsFactories";
+            this.cmsFactories.Size = new System.Drawing.Size(61, 4);
+            this.cmsFactories.Opening += new System.ComponentModel.CancelEventHandler(this.cmsFactories_Opening);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtChannel);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.txtOauth);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(236, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 118);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "OAuth Key:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(73, 19);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(205, 20);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtOauth
+            // 
+            this.txtOauth.Location = new System.Drawing.Point(73, 41);
+            this.txtOauth.Name = "txtOauth";
+            this.txtOauth.Size = new System.Drawing.Size(205, 20);
+            this.txtOauth.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(6, 90);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(272, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtChannel
+            // 
+            this.txtChannel.Location = new System.Drawing.Point(73, 64);
+            this.txtChannel.Name = "txtChannel";
+            this.txtChannel.Size = new System.Drawing.Size(205, 20);
+            this.txtChannel.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Channel:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 396);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lstActiveHandlers);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstActiveHandlers;
+        private System.Windows.Forms.ContextMenuStrip cmsFactories;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtOauth;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtChannel;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OpenBot/frmMain.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenBot
+{
+    public partial class frmMain : Form
+    {
+        private OpenBot _bot;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            _bot = new OpenBot();
+        }
+
+        private void populateList()
+        {
+            lstActiveHandlers.Items.Clear();
+            lstActiveHandlers.BeginUpdate();
+
+            foreach (var i in _bot.PluginManager.PluginAssemblies)
+            {
+                foreach (var u in i.RunningChatHandlersWithNames)
+                {
+                    lstActiveHandlers.Items.Add(u.Key);
+                }
+            }
+
+            lstActiveHandlers.EndUpdate();
+        }
+
+        private void cmsFactories_Opening(object sender, CancelEventArgs e)
+        {
+            cmsFactories.Items.Clear();
+
+            if(lstActiveHandlers.SelectedIndex >= 0)
+            {
+                cmsFactories.Items.Add("Preferences", null, (o, p) =>
+                {
+                    foreach(var i in _bot.PluginManager.PluginAssemblies)
+                    {
+                        foreach (var u in i.RunningChatHandlersWithNames)
+                        {
+                            if (u.Key == (string)lstActiveHandlers.SelectedItem)
+                                u.Value.ShowPreferences();
+                        }
+                    }
+                });
+            }
+
+            foreach(var i in _bot.PluginManager.PluginAssemblies)
+            {
+                foreach(var u in i.Plugins)
+                {
+                    foreach(var f in u.ChatHandlerFactories)
+                    {
+                        cmsFactories.Items.Add(f.Name, null, (o, p) => {
+                            if(!i.CreateChatHandler(f.Name, f))
+                            {
+                                MessageBox.Show("Unable to create \"" + f.Name + "\"", "OpenBot", MessageBoxButtons.OK);
+                            }
+                            populateList();
+                        });
+                    }
+                }
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            _bot.Connect();
+            _bot.DebugOutput = true;
+
+            _bot.Login(txtUsername.Text, txtOauth.Text);
+            _bot.JoinChannel(txtChannel.Text.ToLower());
+
+            _bot.InitializePluginManager();
+            _bot.BeginListen();
+        }
+    }
+}
